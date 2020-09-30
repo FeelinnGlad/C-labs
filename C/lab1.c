3c549b8a69cdc78d9f48bb6aff51c4612d31837b
FeelinnGlad <68422874+FeelinnGlad@users.noreply.github.com>
2020-09-30 22:37:44 +0300

    Create lab1.c

---
+++ C/lab1.c
@@ -0,0 +1,22 @@
+/* Task:
+Expression a - = ((b ++ >> 2! = C) +1) | ++ c (type: int, int, unsigned char). 
+The program should prompt the user for the values of the variables a, b, and c. 
+Evaluate the specified expression and re-display the values of variables a, b, and c.*/
+
+#include <stdio.h>
+
+void main() {
+    int a;
+    int b;
+    unsigned int c;
+    
+    printf("Введите значение a: ");
+    scanf("%d", &a);
+    printf("Введите значение b: ");
+    scanf("%d", &b);
+    printf("Введите значение c: ");
+    scanf("%u", &c);
+    a-=((b++>>2!=c)+1)|++c;
+    printf("%d, %d, %u", a, b, c);
+
+}
