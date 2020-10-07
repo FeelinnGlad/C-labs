4e03fdc226641a8d8baf3a64ce1de5e508495966
FeelinnGlad <68422874+FeelinnGlad@users.noreply.github.com>
2020-10-07 22:50:30 +0300

    Create lab 7.c

---
+++ lab 7.c
@@ -0,0 +1,40 @@
+/* Task:
+Using the combined data type (Struct) implement the program for working with the data according to the task variant. 
+Enter auto price information in the record array and display it as a table.
+Enter string values in a way that allows you to enter text that contains spaces.*/
+
+#include <stdio.h>
+#include <string.h>
+
+struct Car {
+  char name[20];
+  float price;
+};
+
+void main(){
+  int num;
+  char temp;
+  printf("Enter amount of cars: ");
+  scanf("%d", &num);
+  struct Car item[num];
+  for (int n = 0; n < num; n++){
+    printf("Enter model of car %d: ", n + 1);
+    scanf("%c", &temp);
+    scanf("%[^\n]s", &item[n].name);
+    printf("Enter price of car %d: ", n + 1);
+    scanf("%f", &item[n].price);
+  }
+  
+  printf("\n");
+  printf("┌───┬─────────────────────┬─────────────┐\n");
+  printf("│ № │        Model        │ Price mln ¥ │\n");
+  printf("├───┼─────────────────────┼─────────────┤\n");
+  for (int n = 0; n < num; n++){
+    printf("│ %d │ ", n + 1);
+    printf("%s", item[n].name);
+    for (int j = 0; j < 20 - (strlen(item[n].name)); j++) printf(" ");
+    printf("│ %11.4f │\n", item[n].price);
+    printf(n+1 == num ? "└───┴─────────────────────┴─────────────┘\n" : "├───┼─────────────────────┼─────────────┤\n");
+  }
+  
+  }
